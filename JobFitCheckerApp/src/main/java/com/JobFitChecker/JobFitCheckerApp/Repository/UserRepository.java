c3098da13e62b93f15c01ff7064f8ac8e74b8bba
Tiansi Gu <tgu10@dons.usfca.edu>
2024-09-18 22:19:28 -0700

    Implement updateResumeKey RDS API

--- JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/Repository/UserRepository.java
+++ JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/Repository/UserRepository.java
@@ -1,8 +1,19 @@
 package com.JobFitChecker.JobFitCheckerApp.Repository;
 
 import com.JobFitChecker.JobFitCheckerApp.Model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET resume_key = :resumeKey WHERE id = :id", nativeQuery = true)
+    public int updateResumeKey(@Param("id") long id, @Param("resumeKey") String resumeKey);
+
 }
