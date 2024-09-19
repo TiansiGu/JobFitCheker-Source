06ab73d51629f5277d39d48e66c88734378d1825
Tiansi Gu <tgu10@dons.usfca.edu>
2024-09-18 22:20:41 -0700

    Add resumeKey update to putResume and deleteResume

--- JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/Services/UserActivity/ResumeService.java
+++ JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/Services/UserActivity/ResumeService.java
@@ -1,60 +1,68 @@
 package com.JobFitChecker.JobFitCheckerApp.Services.UserActivity;
 
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
+import com.JobFitChecker.JobFitCheckerApp.Repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import static com.JobFitChecker.JobFitCheckerApp.utils.Constant.S3_BUCKET_NAME;
-import static com.JobFitChecker.JobFitCheckerApp.utils.Constant.S3_THREAD_NUM;
 
 @Service
 public final class ResumeService {
     private static final Logger log = LoggerFactory.getLogger(ResumeService.class);
-    private final S3AsyncClient s3AsyncClient;
-    private final ExecutorService executorService;
+
+    private final S3Client s3Client;
 
     @Autowired
-    public ResumeService(S3AsyncClient s3AsyncClient) {
-        this.s3AsyncClient = s3AsyncClient;
-        executorService = Executors.newFixedThreadPool(S3_THREAD_NUM);
+    private UserRepository userRepository;
+
+    @Autowired
+    public ResumeService(S3Client s3AsyncClient) {
+        this.s3Client = s3AsyncClient;
     }
 
-    public CompletableFuture<PutObjectResponse> putResume(MultipartFile file) throws IOException {
-        String key = file.getOriginalFilename(); //ToDo: Add userId to key after login is established
+    public void putResume(long userId, MultipartFile file) throws Exception {
+        String resumeKey = getResumeKey(userId, file.getOriginalFilename());
 
         try {
             PutObjectRequest putRequest = PutObjectRequest.builder()
                     .bucket(S3_BUCKET_NAME)
-                    .key(key)
+                    .key(resumeKey)
                     .contentType(file.getContentType())
                     .build();
-            AsyncRequestBody asyncRequestBody = AsyncRequestBody.fromInputStream(
+            RequestBody requestBody = RequestBody.fromInputStream(
                     file.getInputStream(),
-                    file.getSize(),
-                    executorService
+                    file.getSize()
             );
+            s3Client.putObject(putRequest, requestBody);
 
-            CompletableFuture<PutObjectResponse> response = s3AsyncClient.putObject(putRequest, asyncRequestBody);
+            userRepository.updateResumeKey(userId, resumeKey);
 
-            return response.whenComplete((resp, ex) -> {
-                if (ex != null) {
-                    throw new RuntimeException("Failed to upload file", ex);
-                }
-            });
         } catch (IOException ex) {
-            log.error("Unable to create async request from file" + ex);
+            log.error("Unable to create request body from file" + ex);
             throw ex;
         }
     }
+
+    public void deleteResume(long userId, String resumeKey) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(S3_BUCKET_NAME)
+                .key(resumeKey)
+                .build();
+        s3Client.deleteObject(deleteObjectRequest);
+
+        userRepository.updateResumeKey(userId, null);
+    }
+
+    private String getResumeKey(long userId, String fileName) {
+        return userId + "@" + fileName;
+    }
+
 }
--- JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/modules/AWSSDKModule.java
+++ JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/modules/AWSSDKModule.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AWSSDKModule {
     @Bean
-    public S3AsyncClient getS3AsyncClient() {
-        return S3AsyncClient.builder()
+    public S3Client getS3Client() {
+        return S3Client.builder()
                 .region(Region.US_EAST_2)
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
