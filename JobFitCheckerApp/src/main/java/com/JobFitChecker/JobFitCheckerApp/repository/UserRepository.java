5f9db3d5e5022923cd543e366ac13085e76049b6
Tiansi Gu <tgu10@dons.usfca.edu>
2025-04-21 12:05:33 -0700

    Delete ResumePostProcess workflow in JobFitCheckerApp

--- JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/repository/UserRepository.java
+++ JobFitCheckerApp/src/main/java/com/JobFitChecker/JobFitCheckerApp/repository/UserRepository.java
@@ -22,19 +22,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE users SET degree = :degree, major = :major, graduation_date = :graduationDate, " +
-                    "previous_job_titles = :previousJobTitles, skills = :skills WHERE id = :id", nativeQuery = true)
-    void updateQualification(
-            @Param("id") long id,
-            @Param("degree") String degree,
-            @Param("major") String major,
-            @Param("graduationDate") String graduationDate,
-            @Param("previousJobTitles") String previousJobTitles,
-            @Param("skills") String skills
-    );
-
-    @Modifying
-    @Transactional
     @Query(value = "INSERT INTO application_records (company, position, job_id, create_time, user_id)" +
             "VALUES (:company, :position, :jobId, :createTime, :userId)", nativeQuery = true)
     void addApplicationRecordToUser(
